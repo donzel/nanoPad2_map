4449e7da0a280f5760cf8c5dc29454029ae70033
donze.luc@gmail.com <donze.luc@gmail.com>
2014-02-09 14:12:17 +0100

    Recuperation de l'evenement

--- src/nanopadconf/MidiInputReceiver.java
+++ src/nanopadconf/MidiInputReceiver.java
@@ -3,30 +3,51 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package nanopadconf;
 
 import javax.sound.midi.MidiMessage;
 import javax.sound.midi.Receiver;
+import javax.sound.midi.ShortMessage;
+import static javax.sound.midi.ShortMessage.NOTE_OFF;
+import static javax.sound.midi.ShortMessage.NOTE_ON;
 
 /**
  *
  * @author luc
  */
 //tried to write my own class. I thought the send method handles an MidiEvents sent to it
+public class MidiInputReceiver implements Receiver {
 
-    public class MidiInputReceiver implements Receiver {
+    public String name;
 
-        public String name;
-
-        public MidiInputReceiver(String name) {
-            this.name = name;
-        }
-
-        public void send(MidiMessage msg, long timeStamp) {
-            System.out.println("midi received");
-        }
-
-        public void close() {
-        }
+    public MidiInputReceiver(String name) {
+        this.name = name;
     }
+
+    public void send(MidiMessage msg, long timeStamp) {
+        System.out.println("midi received");
+
+        if (msg instanceof ShortMessage) {
+            ShortMessage sm = (ShortMessage) msg;
+            System.out.print("Channel: " + sm.getChannel() + " ");
+          if (sm.getCommand() == NOTE_ON) {
+                        int key = sm.getData1();                        
+                        int velocity = sm.getData2();
+                        System.out.println("Note on, " + " key=" + key + " velocity: " + velocity);
+                    } else if (sm.getCommand() == NOTE_OFF) {
+                        int key = sm.getData1();                       
+                        int velocity = sm.getData2();
+                        System.out.println("Note off, "+ " key=" + key + " velocity: " + velocity);
+                    } else {
+                        System.out.println("Command:" + sm.getCommand());
+                    }
+                } else {
+                    System.out.println("Other message: " + msg.getClass());
+                }
+        
+
+    }
+
+    public void close() {
+    }
+}
